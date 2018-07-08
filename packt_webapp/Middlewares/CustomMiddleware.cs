ce3018f6d94be6a7714dffd8f699cb7b30201232
David Dassau <todieahero@hotmail.com>
2018-07-08 13:24:47 -0500

    section 2, lecture 7.3

--- packt_webapp/Middlewares/CustomMiddleware.cs
+++ packt_webapp/Middlewares/CustomMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,15 +11,17 @@ namespace packt_webapp.Middlewares
     public class CustomMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly MyConfiguration _myconfig;
 
-        public CustomMiddleware(RequestDelegate next)
+        public CustomMiddleware(RequestDelegate next, IOptions<MyConfiguration> myconfig)
         {
             _next = next;
+            _myconfig = myconfig.Value;
         }
 
         public async Task Invoke(HttpContext httpContext)
         {
-            Debug.WriteLine($" ---> Request asked for {httpContext.Request.Path}");
+            Debug.WriteLine($" ---> Request asked for {httpContext.Request.Path} from {_myconfig.Firstname} {_myconfig.Lastname}");
 
             // Call the next middleware delegate in the pipeline
             await _next.Invoke(httpContext);
